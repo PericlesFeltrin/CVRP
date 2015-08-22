1a5362f02306d91b61be00bb2f7edb3988c2fde3
Péricles Feltrin <Feltrin@Pericless-MBP.home>
2015-08-22 18:26:27 -0300

    Adicionado retorno de parâmetro.

--- arquivo.c
+++ arquivo.c
@@ -1,5 +1,5 @@
-void arquivo(char arq[50], int *cidadeX, int *cidadeY, int *cidadeD){
-	int tamLinha = 100, iLinha = 0, cidades, demanda = 0, capacidade; 
+int arquivo(char arq[50], int *cidadeX, int *cidadeY, int *cidadeD){
+	int tamLinha = 100, iLinha = 0, cidades, capacidade; //, demandaTotal = 0 
 	char linha[tamLinha];
 	char *token;
 	FILE *arquivo;
@@ -18,11 +18,26 @@ void arquivo(char arq[50], int *cidadeX, int *cidadeY, int *cidadeD){
 				token = strtok (NULL, " ");
 				cidades = atoi(token);
 				printf("Quantidade de Cidades: %d \n", cidades);
+				//Alocação do vetor da posição X da cidade
 				cidadeX = malloc((cidades) * sizeof(int));
+				if (cidadeX == NULL) {
+				  printf( "Erro Malloc Cidade X!\n");
+				  exit(-1);
+				}
+				//Alocação do vetor da posição Y da cidade
 				cidadeY = malloc((cidades) * sizeof(int));
+				if (cidadeY == NULL) {
+				  printf( "Erro Malloc Cidade Y!\n");
+				  exit(-1);
+				}
+				//Alocação do vetor das Demandas
 				cidadeD = malloc((cidades) * sizeof(int));
+				if (cidadeD == NULL) {
+				  printf( "Erro Malloc Demanda!\n");
+				  exit(-1);
+				}
 			}else if (iLinha == 5){ 
-				//Capacidade
+				//Capacidade do veículo
 				token = strtok(linha," ");
 				token = strtok (NULL, " ");
 				token = strtok (NULL, " ");
@@ -31,36 +46,34 @@ void arquivo(char arq[50], int *cidadeX, int *cidadeY, int *cidadeD){
 			}else if (iLinha > 6 && iLinha <= 6+cidades){
 				//Cidade X Y
 				int cidade;
+				//Pega a Cidade
 				token = strtok(linha," ");
 				cidade = atoi(token);
 				//printf("Cidade: %d ", atoi(token));
+				//Pega o X
 				token = strtok (NULL, " ");
-				//printf("X: %d ", atoi(token));
 				cidadeX[cidade - 1] = atoi(token);
+				//printf("X: %d ", atoi(token));
+				//Pega o Y
 				token = strtok (NULL, " ");
-				//printf("Y: %d \n", atoi(token));
 				cidadeY[cidade - 1] = atoi(token);
+				//printf("Y: %d \n", atoi(token));
 			}else if (iLinha > (7+cidades) && iLinha <= (7+cidades*2)){
 				//Cidade Demanda
 				int cidade;
+				//Pega a Cidade
 				token = strtok(linha," ");
 				cidade = atoi(token);
 				//printf("Cidade: %s ", token);
+				//Pega a Demanda
 				token = strtok (NULL, " ");
-				//printf("Demanda: %s \n", token);
 				cidadeD[cidade - 1] = atoi(token);
-				demanda += atoi(token);
+				//printf("Demanda: %s \n", token);
+				//demandaTotal += atoi(token);
 			}
 			iLinha++;
 		}
 	}	
 	fclose(arquivo);
-	for (int i = 0; i < cidades; ++i){
-		
-		printf("X: %d ", cidadeX[i]);
-		printf("Y: %d ", cidadeY[i]);
-		printf("D: %d \n", cidadeD[i]);
-	}
-	printf("Demanda: %d \n", demanda );
-	printf("Minimo de carros: %f  \n", ((float)demanda/(float)capacidade) );
+	return capacidade;
 }
